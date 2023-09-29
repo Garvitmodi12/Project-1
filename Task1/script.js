60af3b81ea9068663bc036def71c4b0fdb7dfdd1
Garvitmodi12 <130285018+Garvitmodi12@users.noreply.github.com>
2023-09-29 19:25:02 +0530

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,378 @@
+// ----------toggle-navbar-start
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('open');
+};
+// ----------toggle-navbar-end
+
+
+
+// back to top
+var btn = $('#button');
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+
+
+
+//----- animation js
+AOS.init(
+  {
+    offset: 60,
+    delay: 800,
+    duration: 800,
+    easing: 'ease'
+  }
+);
+
+
+
+// cursor animation
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", function (e) {
+  cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+});
+
+
+
+// preloader js
+setTimeout(function () {
+  $('.loader_bg').fadeToggle();
+}, 1500);
+
+
+
+
+//-------- slider start
+const $window = $(window);
+const $body = $('body');
+
+class Slideshow {
+  constructor(userOptions = {}) {
+    const defaultOptions = {
+      $el: $('.slideshow'),
+      showArrows: false,
+      showPagination: true,
+      duration: 10000,
+      autoplay: true
+    }
+
+    let options = Object.assign({}, defaultOptions, userOptions);
+
+    this.$el = options.$el;
+    this.maxSlide = this.$el.find($('.js-slider-home-slide')).length;
+    this.showArrows = this.maxSlide > 1 ? options.showArrows : false;
+    this.showPagination = options.showPagination;
+    this.currentSlide = 1;
+    this.isAnimating = false;
+    this.animationDuration = 1200;
+    this.autoplaySpeed = options.duration;
+    this.interval;
+    this.$controls = this.$el.find('.js-slider-home-button');
+    this.autoplay = this.maxSlide > 1 ? options.autoplay : false;
+
+    this.$el.on('click', '.js-slider-home-next', (event) => this.nextSlide());
+    this.$el.on('click', '.js-slider-home-prev', (event) => this.prevSlide());
+    this.$el.on('click', '.js-pagination-item', event => {
+      if (!this.isAnimating) {
+        this.preventClick();
+        this.goToSlide(event.target.dataset.slide);
+      }
+    });
+
+    this.init();
+  }
+
+  init() {
+    this.goToSlide(1);
+    if (this.autoplay) {
+      this.startAutoplay();
+    }
+
+    if (this.showPagination) {
+      let paginationNumber = this.maxSlide;
+      let pagination = '<div class="pagination"><div class="container">';
+
+      for (let i = 0; i < this.maxSlide; i++) {
+        let item = `<span class="pagination__item js-pagination-item ${i === 0 ? 'is-current' : ''}" data-slide=${i + 1}>${i + 1}</span>`;
+        pagination = pagination + item;
+      }
+
+      pagination = pagination + '</div></div>';
+
+      this.$el.append(pagination);
+    }
+  }
+
+  preventClick() {
+    this.isAnimating = true;
+    this.$controls.prop('disabled', true);
+    clearInterval(this.interval);
+
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.$controls.prop('disabled', false);
+      if (this.autoplay) {
+        this.startAutoplay();
+      }
+    }, this.animationDuration);
+  }
+
+  goToSlide(index) {
+    this.currentSlide = parseInt(index);
+
+    if (this.currentSlide > this.maxSlide) {
+      this.currentSlide = 1;
+    }
+
+    if (this.currentSlide === 0) {
+      this.currentSlide = this.maxSlide;
+    }
+
+    const newCurrent = this.$el.find('.js-slider-home-slide[data-slide="' + this.currentSlide + '"]');
+    const newPrev = this.currentSlide === 1 ? this.$el.find('.js-slider-home-slide').last() : newCurrent.prev('.js-slider-home-slide');
+    const newNext = this.currentSlide === this.maxSlide ? this.$el.find('.js-slider-home-slide').first() : newCurrent.next('.js-slider-home-slide');
+
+    this.$el.find('.js-slider-home-slide').removeClass('is-prev is-next is-current');
+    this.$el.find('.js-pagination-item').removeClass('is-current');
+
+    if (this.maxSlide > 1) {
+      newPrev.addClass('is-prev');
+      newNext.addClass('is-next');
+    }
+
+    newCurrent.addClass('is-current');
+    this.$el.find('.js-pagination-item[data-slide="' + this.currentSlide + '"]').addClass('is-current');
+  }
+
+  nextSlide() {
+    this.preventClick();
+    this.goToSlide(this.currentSlide + 1);
+  }
+
+  prevSlide() {
+    this.preventClick();
+    this.goToSlide(this.currentSlide - 1);
+  }
+
+  startAutoplay() {
+    this.interval = setInterval(() => {
+      if (!this.isAnimating) {
+        this.nextSlide();
+      }
+    }, this.autoplaySpeed);
+  }
+
+  destroy() {
+    this.$el.off();
+  }
+}
+
+(function () {
+  let loaded = false;
+  let maxLoad = 3000;
+
+  function load() {
+    const options = {
+      showPagination: true
+    };
+
+    let slideShow = new Slideshow(options);
+  }
+
+  function addLoadClass() {
+    $body.addClass('is-loaded');
+
+    setTimeout(function () {
+      $body.addClass('is-animated');
+    }, 600);
+  }
+
+  $window.on('load', function () {
+    if (!loaded) {
+      loaded = true;
+      load();
+    }
+  });
+
+  setTimeout(function () {
+    if (!loaded) {
+      loaded = true;
+      load();
+    }
+  }, maxLoad);
+
+  addLoadClass();
+})();
+//-------- slider end
+
+
+
+
+
+
+// --------testimonial js start
+'use strict'
+var testim = document.getElementById("testim"),
+  testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+  testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+  testimLeftArrow = document.getElementById("left-arrow"),
+  testimRightArrow = document.getElementById("right-arrow"),
+  testimSpeed = 4500,
+  currentSlide = 0,
+  currentActive = 0,
+  testimTimer,
+  touchStartPos,
+  touchEndPos,
+  touchPosDiff,
+  ignoreTouch = 30;
+;
+
+window.onload = function () {
+
+  // Testim Script
+  function playSlide(slide) {
+    for (var k = 0; k < testimDots.length; k++) {
+      testimContent[k].classList.remove("active");
+      testimContent[k].classList.remove("inactive");
+      testimDots[k].classList.remove("active");
+    }
+
+    if (slide < 0) {
+      slide = currentSlide = testimContent.length - 1;
+    }
+
+    if (slide > testimContent.length - 1) {
+      slide = currentSlide = 0;
+    }
+
+    if (currentActive != currentSlide) {
+      testimContent[currentActive].classList.add("inactive");
+    }
+    testimContent[slide].classList.add("active");
+    testimDots[slide].classList.add("active");
+
+    currentActive = currentSlide;
+
+    clearTimeout(testimTimer);
+    testimTimer = setTimeout(function () {
+      playSlide(currentSlide += 1);
+    }, testimSpeed)
+  }
+
+  testimLeftArrow.addEventListener("click", function () {
+    playSlide(currentSlide -= 1);
+  })
+
+  testimRightArrow.addEventListener("click", function () {
+    playSlide(currentSlide += 1);
+  })
+
+  for (var l = 0; l < testimDots.length; l++) {
+    testimDots[l].addEventListener("click", function () {
+      playSlide(currentSlide = testimDots.indexOf(this));
+    })
+  }
+
+  playSlide(currentSlide);
+
+  // keyboard shortcuts
+  document.addEventListener("keyup", function (e) {
+    switch (e.keyCode) {
+      case 37:
+        testimLeftArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      default:
+        break;
+    }
+  })
+
+  testim.addEventListener("touchstart", function (e) {
+    touchStartPos = e.changedTouches[0].clientX;
+  })
+
+  testim.addEventListener("touchend", function (e) {
+    touchEndPos = e.changedTouches[0].clientX;
+
+    touchPosDiff = touchStartPos - touchEndPos;
+
+    console.log(touchPosDiff);
+    console.log(touchStartPos);
+    console.log(touchEndPos);
+
+
+    if (touchPosDiff > 0 + ignoreTouch) {
+      testimLeftArrow.click();
+    } else if (touchPosDiff < 0 - ignoreTouch) {
+      testimRightArrow.click();
+    } else {
+      return;
+    }
+
+  })
+}
+// --------testimonial js start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
